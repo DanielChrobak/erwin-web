ed00cdd86a9b5f9358e302684314b7a0e06a2682
Daniel Chrobak <danielchrobak12@gmail.com>
2024-10-06 17:15:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ async function submitGuesses(apiKey) {
             if (response.response.status === 202) {
                 sleepTime = Math.max(1000, sleepTime - 1000);
             } else if (response.response.status === 429) {
-                sleepTime += 1000;
+                sleepTime += 2000;
             }
         } catch (error) {
             logMessage(`⚠️ Request error | API Key: ${apiKey.slice(0, 10)}... | Error: ${error}`);
